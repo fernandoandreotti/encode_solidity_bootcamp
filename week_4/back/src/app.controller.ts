190bed32ed48451f61e14ed5e5be61c9182ff3b1
0x00Duke <leon.ducasse@epitech.eu>
2023-03-12 23:20:22 +0100

    [DEL] useless / in routes

--- week_4/back/src/app.controller.ts
+++ week_4/back/src/app.controller.ts
@@ -6,17 +6,17 @@ import { RequestTokensDTO } from './dtos/RequestTokensDTO';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get("/contract-address")
+  @Get("contract-address")
   getContractAddress(): { address: string } {
     return { address: this.appService.getContractAddress() };
   }
 
-  @Get("/total-supply")
+  @Get("total-supply")
   async getTotalSupply(): Promise<number> {
     return await this.appService.getTotalSupply();
   }
 
-  @Get("/allowance/")
+  @Get("allowance")
   async getAllowance(
     @Query('from') from: string,
     @Query('to') to: string
@@ -24,19 +24,19 @@ export class AppController {
     return await this.appService.getAllowance(from, to);
   }
 
-  @Get("transaction-status/")
+  @Get("transaction-status")
   async getTransactionStatus(
     @Query('hash') hash: string
   ): Promise<string> {
     return await this.appService.getTransactionStatus(hash);
   }
 
-  @Post("/request-tokens")
+  @Post("request-tokens")
   requestTokens(@Body() body: RequestTokensDTO): void {
     this.appService.requestTokens(body.address, body.amount);
   }
 
-  @Get("/votes")
+  @Get("votes")
   getVotes(): string[] {
     return this.appService.getVotes();
   }
